185d2ac15115df43b8eb32f59a3f6fddfcc27873
VentsislavKostadinov <ventsislav.kostadinov@angloamerican.com>
2024-12-27 15:08:41 +0200

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/react-19-features/', // GitHub repository name
   plugins: [react()],
   //@ts-ignore
   test: {
